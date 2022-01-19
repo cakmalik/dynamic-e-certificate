7355b5119b009cd4b256970a9192cba5c320cb15
hasinilmalik <hasinilmalik@gmail.com>
2022-01-19 15:21:02 +0700

    first commit

--- form_sertifikat.php
+++ form_sertifikat.php
@@ -1,15 +1,17 @@
 <?php
 if(isset($_POST['name'])){
 	$font="BRUSHSCI.TTF";
-	$image=imagecreatefromjpeg("certificate.jpg");
+	$image=imagecreatefromjpeg("sertifikat.jpg");
 	$color=imagecolorallocate($image,19,21,22);
 	$name="Vishal Gupta";
 	imagettftext($image,50,0,365,420,$color,$font,$_POST['name']);
 	$date="6th may 2020";
 	imagettftext($image,20,0,450,595,$color,"AGENCYR.TTF",$date);
 	$file=time();
-	imagejpeg($image,"certificate/".$file.".jpg");
+	imagejpeg($image,"sertifikat/".$file.".jpg");
 	imagedestroy($image);
+
+	
 }
 ?>
 <form method="post">
